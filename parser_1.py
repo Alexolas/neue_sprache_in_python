515eb3f96d8f7b596662e3c6b5369d787917090f
alexolas <nicolas.von.monkiewitsch@igs-heidberg.de>
2021-06-14 09:55:28 +0200

    helpAnimal

--- parser_1.py
+++ parser_1.py
@@ -11,8 +11,10 @@ class Parser:
                 self._messages.append(help_text)
             elif zeile.startswith("say "):
                 said_text = zeile[3:len(zeile)]
-                print(zeile)
-                self._messages.append(said_text.lstrip())
+                self._messages.append(said_text.lstrip()+"\n")
+            elif zeile.lower().startswith("helpanimal") and len(zeile.rstrip()) == 10:
+                self._messages.append(help_animal_text)
+
 
             else:
                 self._messages.append("Du hast mindestens einen Befehl falsch eingegeben. Versuche es mit 'help' für die Liste der Befehle.")
@@ -20,21 +22,21 @@ class Parser:
         return True
 
 # darf:
-# "say ..."
-# "say ... ..."
-# "say ... "
-#
+# "helpAnimal"
+# "helpanimal"
+# "helpAnimal "
 # darf nicht:
-# " say ..."
-# "say..."
-# "say"
-# "say "
+# "helpAnimaltext"
+# "helpAnimal text"
+# " helpAnimal"
+#
 
+help_animal_text = """Alpaka\n- Esel\n- Hase\n- Hund\n- Katze\n- Krokodil\n- Kuh\n- Löwe
+- Panda\n- Salamander\n- Schaf\n- Schwein\n- Tiger\n\n"""
 
 help_text = """say\n- fast gleiche Aufgabe wie print("") in Python 
 - gibt durch Leerzeichen zu trennende Wörter aus\n- gibt folgende Zeichen wieder(a - z, A - Z, 0 - 9, alle
- alle Zeichen)\n- wenn ein Leerzeichen benutzt wird, werden alle
-darauffolgenden Zeichen ignoriert\n- z.B.say Hallo09\n\nmake(=def)\n
+ alle Zeichen)\n- z.B.say Hallo09\n\nmake(=def)\n
 randomAnimal\n- wählt ein zufaelliges Tier aus unserer Liste aus\n- z.B.randomAnimal
 \nrandomAnimal Tiername1, Tiername2, Tiername3, ...\n- wählt ein zufaelliges Tier
 aus\n- du kannst selbst Tiernamen aus unserer Liste angeben\n- die Tiernamen müssen mit
