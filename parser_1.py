b1285126a05b0a4d155bf022c821eef7f0e45a6c
alexolas <nicolas.von.monkiewitsch@igs-heidberg.de>
2021-06-14 10:59:30 +0200

    randomAnimal

--- parser_1.py
+++ parser_1.py
@@ -1,3 +1,4 @@
+import random
 class Parser:
     def __init__(self):
 
@@ -9,30 +10,28 @@ class Parser:
 
             if zeile.startswith("help") and len(zeile.rstrip()) < 5:
                 self._messages.append(help_text)
+
             elif zeile.startswith("say "):
                 said_text = zeile[3:len(zeile)]
                 self._messages.append(said_text.lstrip()+"\n")
-            elif zeile.lower().startswith("helpanimal") and len(zeile.rstrip()) == 10:
-                self._messages.append(help_animal_text)
 
+            elif zeile.startswith("listanimals") or zeile.startswith("listAnimals") and len(zeile.rstrip()) == 11:
+                self._messages.append("Liste:")
+                for l in list_animals:
+                    self._messages.append("\n- " + l)
+                self._messages.append("\n\n")
+
+            elif zeile.startswith("randomAnimal") or zeile.startswith("randomanimal") and len(zeile.rstrip()) == 12:
+                self._messages.append(random.choice(list_animals))
 
             else:
                 self._messages.append("Du hast mindestens einen Befehl falsch eingegeben. Versuche es mit 'help' für die Liste der Befehle.")
                 return False
         return True
 
-# darf:
-# "helpAnimal"
-# "helpanimal"
-# "helpAnimal "
-# darf nicht:
-# "helpAnimaltext"
-# "helpAnimal text"
-# " helpAnimal"
-#
 
-help_animal_text = """Alpaka\n- Esel\n- Hase\n- Hund\n- Katze\n- Krokodil\n- Kuh\n- Löwe
-- Panda\n- Salamander\n- Schaf\n- Schwein\n- Tiger\n\n"""
+list_animals = ['Alpaka','Esel','Hase','Hund','Katze','Krokodil','Kuh','Löwe',
+'Panda','Salamander','Schaf','Schwein','Tiger']
 
 help_text = """say\n- fast gleiche Aufgabe wie print("") in Python 
 - gibt durch Leerzeichen zu trennende Wörter aus\n- gibt folgende Zeichen wieder(a - z, A - Z, 0 - 9, alle
@@ -40,5 +39,5 @@ help_text = """say\n- fast gleiche Aufgabe wie print("") in Python
 randomAnimal\n- wählt ein zufaelliges Tier aus unserer Liste aus\n- z.B.randomAnimal
 \nrandomAnimal Tiername1, Tiername2, Tiername3, ...\n- wählt ein zufaelliges Tier
 aus\n- du kannst selbst Tiernamen aus unserer Liste angeben\n- die Tiernamen müssen mit
-einem Komma getrennt werden\n- z.B.randomAnimal Tiger, Schwein, Huhn\n\nhelpAnimal
+einem Komma getrennt werden\n- z.B.randomAnimal Tiger, Schwein, Huhn\n\nlistAnimals
 - zeigt eine Liste unserer gewaehlten Tiere an\n\n"""
