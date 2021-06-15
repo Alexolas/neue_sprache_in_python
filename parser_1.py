f5161c299160810120d1051898f62f57031a1b16
alexolas <nicolas.von.monkiewitsch@igs-heidberg.de>
2021-06-15 15:00:08 +0200

    eventually finished

--- parser_1.py
+++ parser_1.py
@@ -5,8 +5,8 @@ class Parser:
         self._messages = []
 
     def parse(self, code):
-        list_animals = ['Alpaka', 'Esel', 'Hase', 'Hund', 'Katze', 'Krokodil', 'Kuh', 'Löwe',
-                        'Panda', 'Salamander', 'Schaf', 'Schwein', 'Tiger']
+        list_animals = ['Alpaka', 'Elefant', 'Esel', 'Hase', 'Hund', 'Katze', 'Krokodil', 'Kuh', 'Löwe',
+                        'Maus', 'Panda', 'Salamander', 'Schaf', 'Schwein', 'Tiger']
         variablen = {}
         for zeile in code.split("\n"):
             if zeile.startswith("help") and len(zeile.rstrip()) < 5:
@@ -20,19 +20,24 @@ class Parser:
                 einem Komma getrennt werden\n- z.B.\n<randomAnimal Tiger, Schwein, Huhn\n\nlistAnimals
                 - zeigt eine Liste unserer gewaehlten Tiere an\nz.B.\n<listAnimals\n\n"""
                 self._messages.append(help_text)
-            elif zeile.startswith("say "):
-                said_text = zeile[3:len(zeile)]
-                if said_text.strip() == "$" + variable:
-                    self._messages.append(wert + "\n")
+            elif zeile.startswith("say ") or zeile.startswith("s "):
+                if zeile.startswith("s "):
+                    said_text = zeile[1:len(zeile)].strip()
+                else:
+                    said_text = zeile[3:len(zeile)].strip()
+                if said_text.startswith("$"):
+                    var_name = said_text[1:len(said_text.strip())]
+                    if var_name in variablen:
+                        self._messages.append(variablen[var_name] + "\n")
                 else:
                     self._messages.append(said_text.strip()+"\n")
-            elif zeile.startswith("listanimals") or zeile.startswith("listAnimals") and len(zeile.rstrip()) == 11:
+            elif zeile.startswith("listanimals") or zeile.startswith("listAnimals") or zeile.startswith("la"):
                 self._messages.append("Liste:")
                 for l in list_animals:
                     self._messages.append("\n- " + l)
                 self._messages.append("\n\n")
-            elif zeile.startswith("randomAnimal") or zeile.startswith("randomanimal") and len(zeile.rstrip()) > 11:
-                if len(zeile.rstrip()) == 12:
+            elif zeile.startswith("randomAnimal") or zeile.startswith("randomanimal") or zeile.startswith("ra"):
+                if len(zeile.rstrip()) == 12 or len(zeile.rstrip()) == 2:
                     self._messages.append(random.choice(list_animals))
                 else:
                     if "," not in zeile:
@@ -40,6 +45,8 @@ class Parser:
                         return False
                     if zeile.startswith("randomAnimal"):
                         list = ''.join(zeile.split("randomAnimal "))
+                    elif zeile.startswith("ra"):
+                        list = ''.join(zeile.split("ra "))
                     else:
                         list = ''.join(zeile.split("randomanimal "))
                     for t in list.split(","):
@@ -48,8 +55,11 @@ class Parser:
                             return False
                     e = random.choice(list.split(","))
                     self._messages.append(e.strip() + "\n")
-            elif zeile.startswith("make"):
-                variable_und_wert = ''.join(zeile.split("make "))
+            elif zeile.startswith("mk ") or zeile.startswith("make "):
+                if zeile.startswith("mk") or zeile.startswith("mk "):
+                    variable_und_wert = ''.join(zeile.split("mk "))
+                else:
+                    variable_und_wert = ''.join(zeile.split("make "))
                 if "=" in zeile:
                     variable = variable_und_wert.split("=")[0].strip()
                     wert = variable_und_wert.split("=")[1].strip()
@@ -67,4 +77,3 @@ class Parser:
                 self._messages.append("Du hast wahrscheinlich einen Befehl falsch eingegeben. Versuche es mit 'help' für die Liste der Befehle.\n")
                 return False
         return True
-
